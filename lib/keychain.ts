49ff30c6879ceb85936ac75a3564867366f6b515
Thomas Guyard <t.guyard@gigatribe.com>
2020-06-10 15:54:45 +0200

    Fix RSA key system (pkcs1 vs oaep)

--- lib/keychain.ts
+++ lib/keychain.ts
@@ -1,13 +1,14 @@
 import { LockedKeychain } from './lockedKeychain';
 import { pbkdf2, toBase64, encryptAes, fromBase64, decryptAes } from './utils';
+import { RSAKey } from './jsbn/RSAKey';
 
 export class Keychain {
   private readonly rsaLength = 1024;
   private readonly rsaExp = '10001';
 
-  private password: string;
+  private password: string | null;
 
-  private rsaKeys: CryptoKeyPair | null = null;
+  private rsaKeys: RSAKey | null = null;
   private salt: Uint8Array | null = null;
   private masterKey: Uint8Array | null = null;
   private nodeKey: Uint8Array | null = null;
@@ -17,7 +18,7 @@ export class Keychain {
     salt: Uint8Array;
   } | null = null;
 
-  private constructor(password: string) {
+  private constructor(password: string | null) {
     this.password = password;
   }
 
@@ -28,10 +29,10 @@ export class Keychain {
   }
 
   public static async import(
-    password: string,
-    k: LockedKeychain
+    k: LockedKeychain,
+    password?: string
   ): Promise<Keychain> {
-    const chain = new Keychain(password);
+    const chain = new Keychain(password || null);
     await chain.doImport(k);
     return chain;
   }
@@ -45,29 +46,28 @@ export class Keychain {
 
     if (k.masterKey != null) {
       this.masterKey = fromBase64(k.masterKey);
+    } else if (this.password == null) {
+      throw new Error('Cannot import this keychain without a password');
     } else {
       this.masterKey = await calculateMasterKey(this.password, this.salt);
     }
-
-    console.log(toBase64(this.masterKey));
 
     this.dekInfo = {
       iv: fromBase64(k.rsaKeys.dekInfo.iv),
       salt: fromBase64(k.rsaKeys.dekInfo.salt),
     };
 
-    const privateKey = await this.importPrivateKey(k.rsaKeys.privateKey);
-    const publicKey = await this.importPublicKey(k.rsaKeys.publicKey);
-
-    this.rsaKeys = {
-      privateKey,
-      publicKey,
-    };
+    this.rsaKeys = new RSAKey();
+    await this.importPrivateKey(k.rsaKeys.privateKey);
+    await this.importPublicKey(k.rsaKeys.publicKey);
 
     this.nodeKey = await this.importNodeKey(k.nodeKey);
   }
 
   private async doGenerate() {
+    if (this.password == null) {
+      throw new Error('password must not be null');
+    }
     // generate a salt (the one stored in the user profile)
     this.salt = crypto.getRandomValues(new Uint8Array(96));
 
@@ -75,16 +75,8 @@ export class Keychain {
     this.masterKey = await calculateMasterKey(this.password, this.salt);
 
     // generate the RSA keys
-    this.rsaKeys = await crypto.subtle.generateKey(
-      {
-        name: 'RSA-OAEP',
-        modulusLength: 1024,
-        publicExponent: new Uint8Array([1, 0, 1]),
-        hash: 'SHA-256',
-      },
-      true,
-      ['encrypt', 'decrypt']
-    );
+    this.rsaKeys = new RSAKey();
+    this.rsaKeys.generate(this.rsaLength, this.rsaExp);
 
     // generate the nodeKey
     this.nodeKey = crypto.getRandomValues(new Uint8Array(32));
@@ -123,7 +115,9 @@ export class Keychain {
 
     if (weak) {
       locked.masterKey = masterKey;
-      locked.password = password;
+      if (password != null) {
+        locked.password = password;
+      }
     }
     return locked;
   }
@@ -139,17 +133,15 @@ export class Keychain {
       throw new Error('dekInfo should not be null.');
     }
 
-    const priv = await crypto.subtle.exportKey(
-      'pkcs8',
-      this.rsaKeys.privateKey
-    );
+    const priv = this.rsaKeys.privateKeyToPkcs1PemString();
 
     // Create the key for encoding masterKey
     const masterKey = toBase64(this.masterKey);
     const masterKeyEnc = await pbkdf2(masterKey, this.dekInfo.salt, 1024, 128);
 
     // encode private key
-    const data = new Uint8Array(priv);
+    const enc = new TextEncoder();
+    const data = enc.encode(priv);
 
     const dataEnc = await encryptAes(data, masterKeyEnc, this.dekInfo.iv);
     return toBase64(new Uint8Array(dataEnc.encrypted));
@@ -162,6 +154,9 @@ export class Keychain {
     if (this.dekInfo == null) {
       throw new Error('dekInfo should not be null.');
     }
+    if (this.rsaKeys == null) {
+      throw new Error('rsaKeys should not be null.');
+    }
 
     const pk = fromBase64(pkStr);
     const masterKey = await aesPbkdf2Key(
@@ -173,37 +168,19 @@ export class Keychain {
 
     const dec = new TextDecoder();
     const keyStr = dec.decode(pkDec);
-
-    const reader = new PkcsReader(fromBase64(keyStr));
-    const writer = new PkcsWriter();
-    const translatedKey = writer.writeRsaPrivateKey(
-      reader.readPkcs1RSAPrivate()
-    );
-
-    return await crypto.subtle.importKey(
-      'pkcs8',
-      translatedKey,
-      {
-        name: 'RSA-OAEP',
-        hash: 'SHA-256',
-      },
-      true,
-      ['encrypt', 'decrypt']
-    );
+    try {
+      this.rsaKeys.readPrivateKeyFromPkcs8PemString(keyStr);
+    } catch (e) {
+      this.rsaKeys.readPrivateKeyFromPkcs1PemString(keyStr);
+    }
   }
 
   private async importPublicKey(pkStr: string) {
-    const pk = fromBase64(pkStr);
-    return await crypto.subtle.importKey(
-      'spki',
-      pk,
-      {
-        name: 'RSA-OAEP',
-        hash: 'SHA-256',
-      },
-      true,
-      ['encrypt', 'decrypt']
-    );
+    if (this.rsaKeys == null) {
+      throw new Error('rsaKeys should not be null.');
+    }
+
+    this.rsaKeys.readPublicKeyFromX509PEMString(pkStr);
   }
 
   private async exportPublicKey() {
@@ -211,8 +188,7 @@ export class Keychain {
       throw new Error('rsaKeys should not be null.');
     }
 
-    const pub = await crypto.subtle.exportKey('pkcs8', this.rsaKeys.publicKey);
-    return toBase64(new Uint8Array(pub));
+    return this.rsaKeys.publicKeyToX509PemString();
   }
 
   public async importNodeKey(nk: string) {
@@ -220,17 +196,12 @@ export class Keychain {
       throw new Error('rsaKeys should not be null.');
     }
 
-    console.log(nk);
-    console.log(fromBase64(nk));
+    const tmp = this.rsaKeys.decrypt64(nk);
+    if (tmp == null) {
+      throw new Error('NodeKey decryption failed');
+    }
 
-    let nodeKey = await crypto.subtle.decrypt(
-      {
-        name: 'RSA-OAEP',
-      },
-      this.rsaKeys.privateKey,
-      fromBase64(nk)
-    );
-
+    let nodeKey = fromBase64(tmp);
     if (nodeKey.byteLength > 44) {
       const decoder = new TextDecoder();
       nodeKey = fromBase64(decoder.decode(nodeKey));
@@ -247,27 +218,46 @@ export class Keychain {
       throw new Error('nodeKey should not be null.');
     }
 
-    const encrypted = await crypto.subtle.encrypt(
-      {
-        name: 'RSA-OAEP',
-      },
-      this.rsaKeys.privateKey,
-      this.nodeKey
-    );
-
-    return toBase64(new Uint8Array(encrypted));
+    const dec = new TextDecoder();
+    const tmp = this.rsaKeys.encrypt64(dec.decode(this.nodeKey));
+    if (tmp == null) {
+      throw new Error('NodeKey decryption failed');
+    }
+    return tmp;
   }
 
   // WARNING: this is not compatible with the actual version of GiGa.GG !
-  public async exportLoginPassword() {
+  public async calculateLoginPassword() {
+    if (this.password == null) {
+      throw new Error('password should not be null');
+    }
     const password = await pbkdf2(
       this.password,
       fromBase64('uh7rPXycB9uxLtRHoLFo1OwOyyHr+UTg'),
       512,
-      24
+      192
     );
     return toBase64(password);
   }
+
+  // Compatible version of the loginPassword. WARNING: login is case sensitive
+  public async calculateLoginPasswordCompat(login: string) {
+    if (this.password == null) {
+      throw new Error('password should not be null');
+    }
+    const saltStr = login + '"D<?4\'V%Fh(U,9SjdO4v)|1mJV31]#;W';
+    const enc = new TextEncoder();
+    const salt = enc.encode(saltStr);
+
+    const password = await pbkdf2(
+      this.password,
+      salt,
+      1024,
+      128
+    );
+    return toBase64(password);
+  }
+
 }
 
 async function calculateMasterKey(
@@ -287,306 +277,4 @@ async function aesPbkdf2Key(
   salt: Uint8Array
 ): Promise<Uint8Array> {
   return await pbkdf2(password, salt, 1024, 128);
-}
-
-// function cap64char(str: string) {
-//   const m = str.match(/.{1,64}/g);
-//   if (m == null) {
-//     throw new Error('Bad format: cannot cap empty string');
-//   }
-//   return m.join('\n');
-// }
-
-// function getPrivateKeyStr(privateKey: string) {
-//   let str = '-----BEGIN RSA PRIVATE KEY-----\n';
-//   str += cap64char(privateKey);
-//   str += '\n-----END RSA PRIVATE KEY-----';
-//   return str;
-// }
-
-// function getPublicKeyStr(publicKey: string) {
-//   let str = '-----BEGIN PUBLIC KEY-----\n';
-//   str += cap64char(publicKey);
-//   str += '\n-----END PUBLIC KEY-----';
-//   return str;
-// }
-
-// tslint:disable: no-bitwise
-
-interface RsaPrivate {
-  type: 'rsa-private';
-  n: Uint8Array;
-  e: Uint8Array;
-  d: Uint8Array;
-  iqmp: Uint8Array;
-  p: Uint8Array;
-  q: Uint8Array;
-  dmodp: Uint8Array;
-  dmodq: Uint8Array;
-}
-
-const berInteger = 2;
-
-class PkcsReader {
-  private offset = 0;
-  constructor(private buf: Uint8Array) {}
-
-  private readLength(offset: number) {
-    const oldOffset = offset;
-    // tslint:disable-next-line: no-bitwise
-    let lenB = this.buf[offset] & 0xff;
-    if (lenB === null) {
-      throw new Error('LenB should not be null');
-    }
-
-    offset++;
-
-    // tslint:disable-next-line: no-bitwise
-    if ((lenB & 0x80) === 0x80) {
-      // tslint:disable-next-line: no-bitwise
-      lenB &= 0x7f;
-
-      if (lenB === 0) {
-        throw Error('Indefinite length not supported');
-      }
-
-      if (lenB > 4) {
-        throw Error('encoding too long');
-      }
-
-      if (this.buf.byteLength - offset < lenB) {
-        throw new Error('byteLength - offset should be > lenB');
-      }
-
-      let len = 0;
-      for (let i = 0; i < lenB; i++) {
-        // tslint:disable-next-line: no-bitwise
-        len = (len << 8) + (this.buf[offset++] & 0xff);
-      }
-      return [offset, len];
-    }
-    // Wasn't a variable length
-    return [offset, lenB];
-  }
-
-  private readMPInt(nm: string) {
-    // tslint:disable-next-line: no-bitwise
-    const b = this.buf[this.offset] & 0xff;
-    if (b !== berInteger) {
-      throw new Error(nm + ' is not an Integer: ' + b);
-    }
-    const [offset, len] = this.readLength(this.offset + 1);
-    this.offset = offset;
-    let slice = this.buf.slice(this.offset, this.offset + len);
-    this.offset += len;
-
-    // while (
-    //   slice.length > 1 &&
-    //   slice[0] === 0x00 &&
-    //   (slice[1] & 0x80) === 0x80
-    // ) {
-    //   slice = slice.slice(1);
-    // }
-    return slice;
-  }
-
-  readPkcs1RSAPrivate(): RsaPrivate {
-    const [offset, len] = this.readLength(this.offset + 1);
-    this.offset = offset;
-
-    const version = this.readMPInt('version');
-    // assert.strictEqual(version[0], 0);
-
-    const n = this.readMPInt('modulus');
-    const e = this.readMPInt('public exponent');
-    const d = this.readMPInt('private exponent');
-    const p = this.readMPInt('prime1');
-    const q = this.readMPInt('prime2');
-    const dmodp = this.readMPInt('exponent1');
-    const dmodq = this.readMPInt('exponent2');
-    const iqmp = this.readMPInt('iqmp');
-
-    // now, make the key
-    return {
-      type: 'rsa-private',
-      n,
-      e,
-      d,
-      iqmp,
-      p,
-      q,
-      dmodp,
-      dmodq,
-    };
-
-    // return {
-    //   kty: 'RSA',
-    //   // kid: 'cc34c0a0-bd5a-4a3c-a50d-a2a7db7643df',
-    //   use: 'sig',
-    //   n: toBase64(n),
-    //   e: toBase64(e),
-    //   d: toBase64(d),
-    //   p: toBase64(p),
-    //   q: toBase64(q),
-    //   dp: toBase64(dmodp),
-    //   dq: toBase64(dmodq),
-    //   qi: toBase64(iqmp),
-    // };
-  }
-}
-
-class PkcsWriter {
-  private seq: number[] = [];
-  private data: number[] = [];
-
-  private startSequence(tag: number = 16 + 32) {
-    // sequence = 16 ;
-    // Constructor = 32 ;
-    this.data.push(tag);
-    this.seq.push(this.data.length);
-    console.log('Start seq', this.data.length);
-    // some data will be spliced here.
-  }
-
-  private endSequence() {
-    const seq = this.seq.pop();
-    if (seq == null) {
-      throw new Error('No sequence');
-    }
-    const start = seq;
-    const len = this.data.length - start;
-
-    if (len <= 0x7f) {
-      this.data.splice(seq, 0, len);
-      console.log('End seq with 1 byte', seq, len);
-    } else if (len <= 0xff) {
-      this.data.splice(seq, 0, 0x81, len);
-      console.log('End seq with 2 byte', seq, len);
-    } else if (len <= 0xffff) {
-      console.log('End seq with 3 byte', seq, len);
-      this.data.splice(seq, 0, 0x82, (len >> 8) & 0xff, len & 0xff);
-    } else if (len <= 0xffffff) {
-      console.log('End seq with 4 byte', seq, len);
-      this.data.splice(seq, 0, 0x83, len >> 16, len >> 8, len);
-    } else {
-      throw Error('Sequence too long');
-    }
-  }
-
-  private writeOID(s: string) {
-    const tag = 6; // OID
-
-    const encodeOctet = (b: number[], octet: number) => {
-      if (octet < 128) {
-        b.push(octet);
-      } else if (octet < 16384) {
-        b.push((octet >>> 7) | 0x80);
-        b.push(octet & 0x7f);
-      } else if (octet < 2097152) {
-        b.push((octet >>> 14) | 0x80);
-
-        b.push(((octet >>> 7) | 0x80) & 0xff);
-        b.push(octet & 0x7f);
-      } else if (octet < 268435456) {
-        b.push((octet >>> 21) | 0x80);
-        b.push(((octet >>> 14) | 0x80) & 0xff);
-        b.push(((octet >>> 7) | 0x80) & 0xff);
-        b.push(octet & 0x7f);
-      } else {
-        b.push(((octet >>> 28) | 0x80) & 0xff);
-        b.push(((octet >>> 21) | 0x80) & 0xff);
-        b.push(((octet >>> 14) | 0x80) & 0xff);
-        b.push(((octet >>> 7) | 0x80) & 0xff);
-        b.push(octet & 0x7f);
-      }
-    };
-
-    const tmp = s.split('.');
-    const bytes: number[] = [];
-    bytes.push(parseInt(tmp[0], 10) * 40 + parseInt(tmp[1], 10));
-    tmp.slice(2).forEach((b) => {
-      encodeOctet(bytes, parseInt(b, 10));
-    });
-
-    this.data.push(tag);
-    this.writeLength(bytes.length);
-
-    this.data = this.data.concat(bytes);
-  }
-
-  writeRsaPrivateKey(key: RsaPrivate): Uint8Array {
-    this.startSequence();
-
-    // for private key :
-    this.data.push(2); // integer
-    this.data.push(1); // length
-    this.data.push(0); // data
-
-    {
-      this.startSequence();
-      this.writeOID('1.2.840.113549.1.1.1');
-
-      //
-      this.data.push(5); // Null
-      this.data.push(0); // null value
-
-      this.endSequence();
-    }
-
-    this.startSequence(4); // OctetString
-    this.startSequence();
-
-    // version
-    this.data.push(2); // integer
-    this.data.push(1); // length
-    this.data.push(0); // data
-
-    this.writeBuffer(key.n, 2);
-    this.writeBuffer(key.e, 2);
-    this.writeBuffer(key.d, 2);
-    this.writeBuffer(key.p, 2);
-    this.writeBuffer(key.q, 2);
-    // if (!key.part.dmodp || !key.part.dmodq)
-    // 	utils.addRSAMissing(key);
-    this.writeBuffer(key.dmodp, 2);
-    this.writeBuffer(key.dmodq, 2);
-    this.writeBuffer(key.iqmp, 2);
-
-    this.endSequence();
-    this.endSequence();
-    this.endSequence();
-
-    const pkcs8data = new Uint8Array(this.data);
-    this.data = [];
-    this.seq = [];
-    return pkcs8data;
-  }
-
-  private writeLength(len: number) {
-    if (len <= 0x7f) {
-      this.data.push(len);
-    } else if (len <= 0xff) {
-      this.data.push(0x81);
-      this.data.push(len);
-    } else if (len <= 0xffff) {
-      this.data.push(0x82);
-      this.data.push(len >> 8);
-      this.data.push(len);
-    } else if (len <= 0xffffff) {
-      this.data.push(0x83);
-      this.data.push(len >> 16);
-      this.data.push(len >> 8);
-      this.data.push(len);
-    } else {
-      throw Error('Length too long (> 4 bytes)');
-    }
-  }
-
-  private writeBuffer(buf: Uint8Array, tag: number) {
-    this.data.push(tag);
-    this.writeLength(buf.length);
-    for (const x of buf) {
-      this.data.push(x);
-    }
-  }
 }
--- tests/kechain.test.ts
+++ tests/kechain.test.ts
@@ -1,30 +1,90 @@
 import { expect } from 'chai';
 import { Keychain } from './../lib/keychain';
+import { LockedKeychain } from '../lib/lockedKeychain';
 
 describe('Crypto::Keychain', () => {
+  const importableKeychain: LockedKeychain = {
+    nodeKey:
+      'Am0TSBwizinOPkiECkXY6yWZtZae0xsKU1xSTBjpYTq/1UNVD2ICDicw8JgtYPHqmkdP4bcOCbdPQuGUQ/2mi3oyLsHHcgM2zluCoS8CkzDh7Q9C7yrAspUhGhgHgsAzIxEjbTa4iFeHwez41JGkEC+ucwboE089VlJkhsOxasI=',
+    salt: 'FLOhgPcpK+IzLOqqzUw2Dbe3o47IsDitc2DBQNiU0i8=',
+    rsaKeys: {
+      dekInfo: {
+        type: 'AES-128-CBC:1024',
+        iv: '24etHeLAcj2Wp4n7xesV7Q==',
+        salt: 'g68y7S8Vlwc=',
+      },
+      privateKey:
+        'f/2y9dWg7PolO8kMbn1NSohL9w5djzqzvvJQ6gmvsk1xY3FkU77Q0Xy4Un8SM7grfDiFQtKr2xdTaGivwH0JZRAkdVvuEsYfq8DClyUZLxfVxzAme1Suwpey6Pl/9xpvhKvVt9eyQAlfPg/2llB074ibftPDQpNGpiRBwVvsHRRIC189mWM9UB1wa2bu4ZdHV+/oyxDoojWvXpfvZZjfa+qdrImtkRnQLeGR7aq3eXaZt5Ph0feYmb4viF7g1AE9nsQITSTbgzfnrqLvTwN5oCxlB+keD6bCx/TmK0Dh1G61ouHhZTPu8rcaA66/6MYz/TM+agMoNVrrqNMUZZXlO8fqE69dJsdcENK/2nGYapI+xpQtzRuD24AfvbwsoHfMSDirrN8MR2oWSbzwtkhfv9ULQq0+3H1u3kcwUz/cf/GCyxxcNAi7m1RAkezftLNE8GjJTevRxRT9v+CKh+166vrUClTOhaQH+E5mHB1hEG5jkPwjDkp05LNoEAbftEUkA1dYpDQGfUYJJcksSISeURQUj+mDcnlHGm2ENQa24XYxcSfeR3/eoe/OKmY13rZ3Uy+58XcUf/wkYw4Oz3Dr9/8zqi9wa+vaG7A5swEJXl2l6iP4zOqI7ukIkf2CAKSdLpCLeYH6dKBjHTC3itMEeToWpzdgzc4+JDUWi4inE93tIKUKbVJSmdNz6fSfj03cTKnvYGJWq7HeTH+Qy+0oWd+qZYcWyo4a90SQGc24HRNghs5doS6XjenNuWuVCHjiOQmTrpvqQzcXDaNzOVD++C6u9Fo5Fd4Xk4oOUTw+tgPolv5zV8TSi1avYHOFOCIhriqo8Zwo+UoxdaAp6r4xSU/qnPuUcUhVDYSx/dLyl0sM3y1ePz0lcMhb5wGBSPEfYGJeO7xWFabPXKfN5EuGD9GOSpdAjR4BtfQBkueY7yI/LCui1NJmEYGqLqbY/fWdKVq1N44nrcKqKEl1mi0saOz7dC6+PbcI2etyoICwa8cgCezJ/deaQgVPQV6QAxM31XSp6o584uZlD/c6C+TF9gb/Ma+mHb8hbLa80zb49OZ3VS0F1l9ArBEOuo3dQ7X+rzMXiUm/QdLU/fZeOqsT3w==',
+      publicKey:
+        'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvupt+SoaJxTnyvzo7d2xtVvhe5N2qxhyW/OVNuh/OeWO2T+BZPJsh6n3+YV4mulsAxmbUvvwpL9EI3xynjw5WpSsXgNAg+s2mAbyGTAOLAlPlymBy1R57XA0DgnIfEh3mQsiWP9uaXp8V8PNkWNC6d+JtRUEImDFy+mFI5XIcQwIDAQAB',
+    },
+  };
+
   it('should generate a keychain', async () => {
     const keychain = await Keychain.generate('gigatribe');
     expect(keychain).not.equal(null);
   });
 
   it('should import a keychain', async () => {
-    const keychain = await Keychain.import('gigatribe', {
-      nodeKey:
-        'Am0TSBwizinOPkiECkXY6yWZtZae0xsKU1xSTBjpYTq/1UNVD2ICDicw8JgtYPHqmkdP4bcOCbdPQuGUQ/2mi3oyLsHHcgM2zluCoS8CkzDh7Q9C7yrAspUhGhgHgsAzIxEjbTa4iFeHwez41JGkEC+ucwboE089VlJkhsOxasI=',
-      salt: 'FLOhgPcpK+IzLOqqzUw2Dbe3o47IsDitc2DBQNiU0i8=',
-      rsaKeys: {
-        dekInfo: {
-          type: 'AES-128-CBC:1024',
-          iv: '24etHeLAcj2Wp4n7xesV7Q==',
-          salt: 'g68y7S8Vlwc=',
-        },
-        privateKey:
-          'f/2y9dWg7PolO8kMbn1NSohL9w5djzqzvvJQ6gmvsk1xY3FkU77Q0Xy4Un8SM7grfDiFQtKr2xdTaGivwH0JZRAkdVvuEsYfq8DClyUZLxfVxzAme1Suwpey6Pl/9xpvhKvVt9eyQAlfPg/2llB074ibftPDQpNGpiRBwVvsHRRIC189mWM9UB1wa2bu4ZdHV+/oyxDoojWvXpfvZZjfa+qdrImtkRnQLeGR7aq3eXaZt5Ph0feYmb4viF7g1AE9nsQITSTbgzfnrqLvTwN5oCxlB+keD6bCx/TmK0Dh1G61ouHhZTPu8rcaA66/6MYz/TM+agMoNVrrqNMUZZXlO8fqE69dJsdcENK/2nGYapI+xpQtzRuD24AfvbwsoHfMSDirrN8MR2oWSbzwtkhfv9ULQq0+3H1u3kcwUz/cf/GCyxxcNAi7m1RAkezftLNE8GjJTevRxRT9v+CKh+166vrUClTOhaQH+E5mHB1hEG5jkPwjDkp05LNoEAbftEUkA1dYpDQGfUYJJcksSISeURQUj+mDcnlHGm2ENQa24XYxcSfeR3/eoe/OKmY13rZ3Uy+58XcUf/wkYw4Oz3Dr9/8zqi9wa+vaG7A5swEJXl2l6iP4zOqI7ukIkf2CAKSdLpCLeYH6dKBjHTC3itMEeToWpzdgzc4+JDUWi4inE93tIKUKbVJSmdNz6fSfj03cTKnvYGJWq7HeTH+Qy+0oWd+qZYcWyo4a90SQGc24HRNghs5doS6XjenNuWuVCHjiOQmTrpvqQzcXDaNzOVD++C6u9Fo5Fd4Xk4oOUTw+tgPolv5zV8TSi1avYHOFOCIhriqo8Zwo+UoxdaAp6r4xSU/qnPuUcUhVDYSx/dLyl0sM3y1ePz0lcMhb5wGBSPEfYGJeO7xWFabPXKfN5EuGD9GOSpdAjR4BtfQBkueY7yI/LCui1NJmEYGqLqbY/fWdKVq1N44nrcKqKEl1mi0saOz7dC6+PbcI2etyoICwa8cgCezJ/deaQgVPQV6QAxM31XSp6o584uZlD/c6C+TF9gb/Ma+mHb8hbLa80zb49OZ3VS0F1l9ArBEOuo3dQ7X+rzMXiUm/QdLU/fZeOqsT3w==',
-        publicKey:
-          'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCvupt+SoaJxTnyvzo7d2xtVvhe5N2qxhyW/OVNuh/OeWO2T+BZPJsh6n3+YV4mulsAxmbUvvwpL9EI3xynjw5WpSsXgNAg+s2mAbyGTAOLAlPlymBy1R57XA0DgnIfEh3mQsiWP9uaXp8V8PNkWNC6d+JtRUEImDFy+mFI5XIcQwIDAQAB',
-      },
-    });
-
+    const keychain = await Keychain.import(importableKeychain, 'gigatribe');
     expect(keychain).not.equal(null);
   });
+
+  it('should export a keychain', async () => {
+    const keychain = await Keychain.import(importableKeychain, 'gigatribe');
+    let exported = await keychain.export(true);
+
+    expect(exported.rsaKeys.publicKey).equal(importableKeychain.rsaKeys.publicKey);
+    expect(exported.rsaKeys.dekInfo.iv).equal(importableKeychain.rsaKeys.dekInfo.iv);
+    expect(exported.rsaKeys.dekInfo.salt).equal(importableKeychain.rsaKeys.dekInfo.salt);
+    expect(exported.rsaKeys.dekInfo.type).equal(importableKeychain.rsaKeys.dekInfo.type);
+    expect(exported.salt).equal(importableKeychain.salt);
+    // weak
+    expect(exported.masterKey).equal('jELo/+hD23tTN1/tsGSeHw==');
+    expect(exported.password).equal('gigatribe');
+
+    exported = await keychain.export(false);
+    expect(exported.rsaKeys.publicKey).equal(importableKeychain.rsaKeys.publicKey);
+    expect(exported.rsaKeys.dekInfo.iv).equal(importableKeychain.rsaKeys.dekInfo.iv);
+    expect(exported.rsaKeys.dekInfo.salt).equal(importableKeychain.rsaKeys.dekInfo.salt);
+    expect(exported.rsaKeys.dekInfo.type).equal(importableKeychain.rsaKeys.dekInfo.type);
+    expect(exported.salt).equal(importableKeychain.salt);
+    // strong
+    expect(exported.masterKey).equal(undefined);
+    expect(exported.password).equal(undefined);
+  });
+
+  it('should import a generated keychain', async () => {
+    const keychain = await Keychain.generate('gigatribe');
+    const exp1 = await keychain.export(true);
+    const imported = await Keychain.import(exp1, 'gigatribe');
+    const exported = await imported.export(true);
+
+    expect(exported.rsaKeys.publicKey).equal(exp1.rsaKeys.publicKey);
+    expect(exported.rsaKeys.dekInfo.iv).equal(exp1.rsaKeys.dekInfo.iv);
+    expect(exported.rsaKeys.dekInfo.salt).equal(exp1.rsaKeys.dekInfo.salt);
+    expect(exported.rsaKeys.dekInfo.type).equal(exp1.rsaKeys.dekInfo.type);
+    expect(exported.salt).equal(exp1.salt);
+    // weak
+    // expect(exported.masterKey).equal(random string here !);
+    expect(exported.password).equal('gigatribe');
+  });
+
+  it('should fail import when the password is wrong', async () => {
+    const keychain = await Keychain.generate('gigatribe');
+    const exp1 = await keychain.export(false);
+
+    try {
+      await Keychain.import(exp1, 'wrong password');
+      expect(true).equal(false);
+    } catch (error) {
+    }
+  });
+
+  it('should generate the login password correctly', async () => {
+    const keychain = await Keychain.generate('azertyuiop');
+    const lp = await keychain.calculateLoginPasswordCompat('mobiuser01');
+    expect(lp).equal('Ju51bwKeziurk32HMdVx8g==');
+  });
+
 });
